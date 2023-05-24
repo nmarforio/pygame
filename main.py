fc5db90230a4df56b430a9dd55fd7b5803d18d4a
python-game <n.marforio@gmail.com>
2023-05-24 17:19:31 +0200

    bullet work now properly

--- main.py
+++ main.py
@@ -30,14 +30,18 @@ enemyX = random.randint(0,736)
 enemyY = random.randint(50, 150)
 enemyX_change = 1
 enemyY_change = 40
+enemy_state = "alive"
 
 def enemy(x,y):
-    screen.blit(enemyImg,(enemyX,enemyY))
+    if enemy_state == "alive":
+        screen.blit(enemyImg,(enemyX,enemyY))
+    if enemy_state == "dead":
+        print ("GOOD JOB")
+    
 
 # bullet 
 bulletImg = pygame.image.load('bullet.png')
 bulletY = 480
-bulletX = 0
 bulletY_change = -2
 bullet_state = "ready"  #ready you dont see the bullet / fire yes
 
@@ -45,7 +49,16 @@ bullet_state = "ready"  #ready you dont see the bullet / fire yes
 def fire_bullet(x,y):
     global bullet_state
     bullet_state = "fire"
-    screen.blit(bulletImg,(x,y))
+    screen.blit(bulletImg,(x, y))
+
+# def kill_monster(xmonster,ymonster,xbullet,ybullet):
+#     if xmonster and ymonster == xbullet and ybullet:
+#         global bullet_state
+#         global enemy_state
+#         bullet_state = "ready" 
+#         enemy_state = "dead"
+
+
     
 
 # Game running
@@ -63,15 +76,19 @@ while running:
 # keystroke to move the spaceship and bullets
     if event.type == pygame.KEYDOWN:
         if event.key == pygame.K_UP:
-            fire_bullet(playerX,bulletY)
+            if bullet_state == 'ready':
+                bulletX = playerX
+                fire_bullet(bulletX,bulletY)
+
+            
         if event.key == pygame.K_LEFT:
             playerX_change -= 2
         elif event.key == pygame.K_RIGHT:
             playerX_change += 2
     if event.type == pygame.KEYUP:
-        if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT or event.key == pygame.K_UP:
+        if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
             playerX_change = 0
-            fire_bullet(playerX,bulletY)
+       
   
       
     
@@ -95,8 +112,14 @@ while running:
         enemyY += enemyY_change
 
 # bullet movements 
+    if bulletY <= 0:
+        bulletY = 480
+        bullet_state = 'ready'
     if bullet_state is 'fire':
+        fire_bullet(bulletX, bulletY)
         bulletY += bulletY_change
+        
+        
     
        
 # called function
