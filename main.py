647e07c95128f7388bfc11a03e09dd5b0e588ecc
python-game <n.marforio@gmail.com>
2023-05-24 12:02:42 +0200

    bullet dosent wor

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ def player (x,y):
 
 # enemy
 enemyImg = pygame.image.load("monster.png")
-enemyX = random.randint(0,768)
+enemyX = random.randint(0,736)
 enemyY = random.randint(50, 150)
 enemyX_change = 1
 enemyY_change = 40
@@ -34,19 +34,35 @@ enemyY_change = 40
 def enemy(x,y):
     screen.blit(enemyImg,(enemyX,enemyY))
 
-# Game running
+# bullet 
+bulletImg = pygame.image.load('bullet.png')
+bulletY = 480
+bulletX = 0
+bulletY_change = 10
 
+bullet_state = "ready"  #ready you dont see the bullet / fire yes
+
+def fire_bullet(x,y):
+    global bullet_state
+    bullet_state = "fire"
+    screen.blit(bulletImg,(x + 16, y + 10))
+
+# Game running
 running = True
 while running:
     screen.fill((0,0,0))
     screen.blit(background,(0,0))
+   
     
-
+# to quit the game
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-# keystroke to move the spaceship
+
+# keystroke to move the spaceship and bullets
     if event.type == pygame.KEYDOWN:
+        if event.key == pygame.K_SPACE:
+            fire_bullet(playerX, bulletY)
         if event.key == pygame.K_LEFT:
             playerX_change -= 2
         elif event.key == pygame.K_RIGHT:
@@ -54,6 +70,7 @@ while running:
     if event.type == pygame.KEYUP:
         if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
             playerX_change = 0
+    
     
 
 # player boundries
@@ -63,15 +80,24 @@ while running:
     elif playerX >= 736:
         playerX = 736
 
-# enemy movement change
+
+# enemy movements
     enemyX += enemyX_change
     if enemyX <= 0:
         enemyX_change = 1
         enemyY += enemyY_change
-    elif enemyX >= 768:
+    elif enemyX >= 736:
         enemyX_change = -1
         enemyY += enemyY_change
 
+# bullet movements 
+    if bullet_state == "fire":
+        fire_bullet(playerX,bulletY)
+        bulletY += bulletY_change
+    
+       
+# called function
+    
     player(playerX,playerY)
     enemy(enemyX,enemyY)
     pygame.display.update()
