f2c4db4635779a8c4636a13897528e254c97648b
python-game <n.marforio@gmail.com>
2023-05-24 10:51:06 +0200

    game with the enemy and player next the bullets

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 # doing my first commit
 
 import pygame
-
+import random
 # initialisation 
 pygame.init()
 
 #screen
-
 screen = pygame.display.set_mode((800,600))
+# background
+background = pygame.image.load('background.jpg')
 
 #caption and Icon
 pygame.display.set_caption("Space Invaders")
@@ -23,11 +24,22 @@ playerX_change = 0
 def player (x,y):
     screen.blit(playerImg, (x,y))
 
+# enemy
+enemyImg = pygame.image.load("monster.png")
+enemyX = random.randint(0,768)
+enemyY = random.randint(50, 150)
+enemyX_change = 1
+enemyY_change = 40
+
+def enemy(x,y):
+    screen.blit(enemyImg,(enemyX,enemyY))
+
 # Game running
 
 running = True
 while running:
     screen.fill((0,0,0))
+    screen.blit(background,(0,0))
     
 
     for event in pygame.event.get():
@@ -36,13 +48,30 @@ while running:
 # keystroke to move the spaceship
     if event.type == pygame.KEYDOWN:
         if event.key == pygame.K_LEFT:
-            playerX -= 0.5
+            playerX_change -= 2
         elif event.key == pygame.K_RIGHT:
-            playerX += 0.5
+            playerX_change += 2
+    if event.type == pygame.KEYUP:
+        if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+            playerX_change = 0
     
 
-    
+# player boundries
+    playerX += playerX_change
+    if playerX <= 0:
+        playerX = 0
+    elif playerX >= 736:
+        playerX = 736
 
-    playerX_change + playerX
+# enemy movement change
+    enemyX += enemyX_change
+    if enemyX <= 0:
+        enemyX_change = 1
+        enemyY += enemyY_change
+    elif enemyX >= 768:
+        enemyX_change = -1
+        enemyY += enemyY_change
+
     player(playerX,playerY)
+    enemy(enemyX,enemyY)
     pygame.display.update()
