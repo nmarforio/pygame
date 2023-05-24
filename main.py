f3f80816854ccc49c3b2d4f7f08c288eaa203ee7
python-game <n.marforio@gmail.com>
2023-05-24 16:30:26 +0200

    bullet works

--- main.py
+++ main.py
@@ -38,14 +38,15 @@ def enemy(x,y):
 bulletImg = pygame.image.load('bullet.png')
 bulletY = 480
 bulletX = 0
-bulletY_change = 10
-
+bulletY_change = -2
 bullet_state = "ready"  #ready you dont see the bullet / fire yes
+
 
 def fire_bullet(x,y):
     global bullet_state
     bullet_state = "fire"
-    screen.blit(bulletImg,(x + 16, y + 10))
+    screen.blit(bulletImg,(x,y))
+    
 
 # Game running
 running = True
@@ -61,18 +62,21 @@ while running:
 
 # keystroke to move the spaceship and bullets
     if event.type == pygame.KEYDOWN:
-        if event.key == pygame.K_SPACE:
-            fire_bullet(playerX, bulletY)
+        if event.key == pygame.K_UP:
+            fire_bullet(playerX,bulletY)
         if event.key == pygame.K_LEFT:
             playerX_change -= 2
         elif event.key == pygame.K_RIGHT:
             playerX_change += 2
     if event.type == pygame.KEYUP:
-        if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+        if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT or event.key == pygame.K_UP:
             playerX_change = 0
+            fire_bullet(playerX,bulletY)
+  
+      
     
     
-
+ 
 # player boundries
     playerX += playerX_change
     if playerX <= 0:
@@ -91,8 +95,7 @@ while running:
         enemyY += enemyY_change
 
 # bullet movements 
-    if bullet_state == "fire":
-        fire_bullet(playerX,bulletY)
+    if bullet_state is 'fire':
         bulletY += bulletY_change
     
        
