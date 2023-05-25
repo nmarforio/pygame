68c1460f1a802d1be704d49745ef42ee4c2d8ed2
python-game <n.marforio@gmail.com>
2023-05-25 10:33:32 +0200

    add sound

--- main.py
+++ main.py
@@ -3,21 +3,31 @@
 import pygame
 import random
 import math
+from pygame import mixer
 # initialisation 
 pygame.init()
 
-#screen
-screen = pygame.display.set_mode((800,600))
+#screen --> creating the high and the width of the window
+screen = pygame.display.set_mode((800,600)) 
 # background
 background = pygame.image.load('background.jpg')
+#background music
+mixer.music.load('background.mp3')
+mixer.music.play(-1)
 
-#caption and Icon
+#caption and Icon  --> to change the Icon when you open the program
 pygame.display.set_caption("Space Invaders")
 icon = pygame.image.load("arcade.png")
 pygame.display.set_icon(icon)
 
 # score
-score = 0
+score_value= 0
+font = pygame.font.Font('freesansbold.ttf', 32) #freesans only free font in pygame, 32 is the size
+textX = 10
+textY = 540
+def show_score (x,y):
+    score = font.render('Score: '+ str(score_value), True, (255,255,255))
+    screen.blit(score,(x,y))
 
 # player
 playerImg = pygame.image.load("spaceship.png")
@@ -26,18 +36,26 @@ playerY = 480
 playerX_change = 0
 
 def player (x,y):
-    screen.blit(playerImg, (x,y))
+    screen.blit(playerImg, (x,y))  #blit is to draw in your window without nothing is showing 
 
-# enemy
-enemyImg = pygame.image.load("monster.png")
-enemyX = random.randint(0,736)
-enemyY = random.randint(50, 150)
-enemyX_change = 1
-enemyY_change = 40
+# list of enemy --> is the sqarebrakets if you wanna use it must be .append in a for loop
+enemyImg = []
+enemyX = []
+enemyY = []
+enemyX_change = []
+enemyY_change = []
+num_enemies = 6
+
+for i in range (num_enemies):
+    enemyImg.append(pygame.image.load("monster.png"))
+    enemyX.append(random.randint(0,736))
+    enemyY.append(random.randint(50, 150))
+    enemyX_change.append(1) 
+    enemyY_change.append(40)
 
 
-def enemy(x,y):
-        screen.blit(enemyImg,(enemyX,enemyY))
+def enemy(x, y ,i):
+        screen.blit(enemyImg[i],(x,y))
     
 
 # bullet 
@@ -61,12 +79,14 @@ def kill_monster(monsterX,monsterY,bulletX,bulletY):
         return False
     
 
+   
     
 # Game running
 running = True
 while running:
     screen.fill((0,0,0))
     screen.blit(background,(0,0))
+    show_score(textX,textY)
    
     
 # to quit the game
@@ -78,6 +98,8 @@ while running:
     if event.type == pygame.KEYDOWN:
         if event.key == pygame.K_UP:
             if bullet_state == 'ready':
+                bullet_sound = mixer.Sound('laser.wav')
+                bullet_sound.play()
                 bulletX = playerX
                 fire_bullet(bulletX,bulletY)
                 
@@ -106,13 +128,26 @@ while running:
 
 
 # enemy movements
-    enemyX += enemyX_change
-    if enemyX <= 0:
-        enemyX_change = 1
-        enemyY += enemyY_change
-    elif enemyX >= 736:
-        enemyX_change = -1
-        enemyY += enemyY_change
+    for i in range (num_enemies):
+        enemyX[i] += enemyX_change[i] #the [i] is to know which of the 6 enemies we are pointing that's why is everywhere 
+        if enemyX[i] <= 0:
+            enemyX_change[i] = 1
+            enemyY[i] += enemyY_change[i]
+        elif enemyX[i] >= 736:
+            enemyX_change[i] = -1
+            enemyY[i] += enemyY_change[i]
+        # collision
+        collision = kill_monster(enemyX[i],enemyY[i],bulletX,bulletY)
+        if collision:
+            collision_sound= mixer.Sound('kill.mp3')
+            collision_sound.play()
+            bulletY = 480
+            bullet_state = 'ready'     
+            score_value += 1
+            enemyX[i] = random.randint(0,736)
+            enemyY[i] = random.randint(50, 150)
+        enemy(enemyX[i], enemyY[i], i)
+    
 
 # bullet movements 
     if bulletY <= 0:
@@ -122,18 +157,8 @@ while running:
         fire_bullet(bulletX, bulletY)
         bulletY += bulletY_change
        
-# collision
-    collision = kill_monster(enemyX,enemyY,bulletX,bulletY)
-    if collision:
-        bulletY = 480
-        bullet_state = 'ready'     
-        score += 1
-        print(score)
-        enemyX = random.randint(0,736)
-        enemyY = random.randint(50, 150)
-    
+
        
 # called function
     player(playerX,playerY)
-    enemy(enemyX,enemyY)
     pygame.display.update()
